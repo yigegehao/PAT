0e439c999e180b41d309ea07643013b3201a33af
yigegehao <1119082433@qq.com>
2018-03-22 21:00:39 +0800

    add 1017

---
+++ 1017.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void deal(string A,int B)
+{
+    if(A.size()==1)
+    {
+         cout<<(A[0]-'0')/B<<" "<<(A[0]-'0')%B<<endl;
+         return;
+    }
+
+    vector<int> v;
+    int t,Q,R,left=0;
+    for(int i=0;i<A.size();i++)
+    {
+            t=A[i]-'0'+left;
+            v.push_back(t/B);
+            if(i==A.size()-1)
+                left=t%B;
+            else
+                left=(t%B)*10;
+    }
+    for(int j=0;j<v.size();j++)
+    {
+        if(j==0&&v[0]==0)
+            ;
+        else
+            cout<<v[j];
+    }
+    cout<<" "<<left<<endl;
+}
+int main()
+{
+    string A;
+    int B;
+    cin>>A>>B;
+    deal(A,B);
+}
---
+++ 1017.py
@@ -0,0 +1,5 @@
+
+a,b = map(int,input().split())
+
+#print('{:d}'.format(int(a/b)),a%b)
+print("%.21f"%(a/b))
